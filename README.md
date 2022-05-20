fd5a1f15f0411b1b289fc8c7fca909ee021f4f6f
zahraeln17 <zahra.elgysha@upi.edu>
2022-05-20 17:19:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# info_upi
+Untuk quiz 2 Flutter
---
+++ lib/Fakultas.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'RincianFakultas.dart';
+
+class Fakultas extends StatelessWidget {
+  const Fakultas({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
+        InkWell(
+          child: Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Text("baris 1"),
+          ),
+          onTap: () {
+            //gunakan navigator untuk panggil RincianFakultas
+          },
+        ),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Text("baris kedua")),
+      ]),
+    );
+  }
+}

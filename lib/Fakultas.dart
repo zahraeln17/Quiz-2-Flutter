dc98c06d932ab29a22338b6cdc22281beeb0b616
Zahra Elgysha <79628357+zahraeln17@users.noreply.github.com>
2022-05-20 18:06:02 +0700

    Update Fakultas.dart

--- lib/Fakultas.dart
+++ lib/Fakultas.dart
@@ -12,8 +12,30 @@ class Fakultas extends StatelessWidget {
           child: Container(
             decoration: BoxDecoration(border: Border.all()),
             padding: EdgeInsets.all(14),
-            child: Text("baris 1"),
-          ),
+            child: Row(
+                children: [
+                  Container(
+                      child: Column(
+                    children: [
+                      Text("FPMIPA",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text("Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  )),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'))),
+                  height: 100,
+                    width: 100,
+                  )
+                ],
+              )
           onTap: () {
             //gunakan navigator untuk panggil RincianFakultas
           },
